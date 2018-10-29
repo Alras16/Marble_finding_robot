c872e2d3c5a8d3c22f6c3b71a666303e19428cdd
marcus-grove <mgrov16@student.sdu.dk>
2018-10-29 09:47:32 +0100

    map_class.h

---
+++ code/map_class.cpp
@@ -0,0 +1,126 @@
+#include "map_class.h"
+
+map_class::map_class(std::string fileName, int numb_of_rooms, bool setAsTree)
+{
+    asTree = setAsTree;
+    set_image(fileName);
+    image_w = ori_map.cols; // cols
+    image_h = ori_map.rows; // rows
+    currLocation.x = image_w / 2;
+    currLocation.y = image_h / 2;
+    init_rooms(numb_of_rooms);
+    cv::Vec3i color = {255,255,255};
+    for (int i = 0; i < numb_of_rooms; i++)
+        paintRoom(listOfRooms[i],color);
+}
+
+std::vector<map_class::room*> map_class::getRooms()
+{
+    return listOfRooms;
+}
+
+void map_class::showMap(std::string title)
+{
+    cv::Mat temp = ori_map.clone();
+    cv::circle(temp,currLocation,6,cv::Scalar({255,255,0}),-1);
+    cv::imshow(title,temp);
+}
+
+cv::Point map_class::convertToPixel(point aPoint)
+{
+    int pixelX = round((aPoint.x*image_w)/WORLD_WIDTH) + (image_w / 2);
+    int pixelY = round((aPoint.y*image_h)/WORLD_HEIGHT) + (image_h / 2);
+    return cv::Point(pixelX,pixelY);
+}
+
+map_class::point map_class::convertToCoordinate(cv::Point pixel)
+{
+    point temp;
+    temp.x = ((pixel.x*WORLD_WIDTH)/image_w) - (WORLD_WIDTH / 2);
+    temp.y = ((pixel.y*WORLD_HEIGHT)/image_h) - (WORLD_HEIGHT / 2);
+    return temp;
+}
+
+void map_class::setCurrentLocation(cv::Point coordinate)
+{
+    currLocation = coordinate;
+}
+
+cv::Point map_class::getCurrentLocation()
+{
+    return currLocation;
+}
+
+void map_class::set_image(std::string fileName)
+{
+    QDir path = QDir::current();
+    path.cdUp();
+    std::string filePath = path.path().toStdString() + "/test_files/maps/" + fileName;
+    ori_map = cv::imread(filePath,cv::IMREAD_COLOR);
+}
+
+std::vector<cv::Point> map_class::find_color(int color)
+{
+    std::vector<cv::Point> points;
+    for (int i = 0; i < image_h; i++)
+        for (int j = 0; j < image_w; j++)
+        {
+            cv::Vec3b cur_pixel = *ori_map.ptr<cv::Vec3b>(i,j);
+            if ((int(cur_pixel[0]) == color) && (int(cur_pixel[1]) == color) && (int(cur_pixel[2]) == color))
+                points.push_back(cv::Point(j,i));
+        }
+    return points;
+}
+
+void map_class::init_rooms(int numb_of_rooms)
+{
+    int color = 245;
+    int roomNumb = 1;
+
+    if (asTree)
+    {
+        for (int i = 0; i < numb_of_rooms; i++)
+        {
+            room * tempRoom = new room;
+            tempRoom->roomNumber = roomNumb++;
+            tempRoom->probabilityOfMarbles = 0.0;
+            std::vector<cv::Point> temp = find_color(color);
+            tempRoom->coordinatesTree.generateCompleteTree(temp);
+            listOfRooms.push_back(tempRoom);
+            color -= 10;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < numb_of_rooms; i++)
+        {
+            room * tempRoom = new room;
+            tempRoom->roomNumber = roomNumb++;
+            tempRoom->probabilityOfMarbles = 0.0;
+            tempRoom->coordinates = find_color(color);
+            listOfRooms.push_back(tempRoom);
+            color -= 10;
+        }
+    }
+}
+
+void map_class::paintRoom(room* theRoom, cv::Vec3i color)
+{
+    std::vector<cv::Point> temp;
+    if (asTree)
+        temp = theRoom->coordinatesTree.convertToList();
+    else
+        temp = theRoom->coordinates;
+
+    unsigned int numbOfPoints = temp.size();
+    for (unsigned int i = 0; i < numbOfPoints; i++)
+    {
+        cv::Point point = temp[i];
+        *ori_map.ptr<cv::Vec3b>(point.y,point.x) = color;
+    }
+}
+
+void map_class::paintMarble(foundMarble marble)
+{
+    cv::circle(ori_map,marble.center,5,cv::Scalar({255,0,0}),-1);
+}
---
+++ code/map_class.h
@@ -0,0 +1,67 @@
+#ifndef MAP_CLASS_H
+#define MAP_CLASS_H
+
+#define WORLD_WIDTH 8.47
+#define WORLD_HEIGHT 5.64
+
+#include <QCoreApplication>
+#include <QDir>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "bst_coordinates.h"
+
+class map_class
+{
+public:
+
+    struct point
+    {
+        float x;
+        float y;
+    };
+
+    struct room
+    {
+        int roomNumber;
+        std::vector<cv::Point> coordinates;
+        BST_coordinates coordinatesTree;
+        float probabilityOfMarbles;
+    };
+    
+    struct foundMarble
+    {
+        cv::Point center;
+        room *foundInRoom;
+    };
+
+    map_class(std::string fileName, int numb_of_rooms, bool setAsTree);
+
+    std::vector<room*> getRooms();
+    void showMap(std::string title);
+    cv::Point convertToPixel(point aPoint);
+    point convertToCoordinate(cv::Point pixel);
+    void setCurrentLocation(cv::Point coordinate);
+    cv::Point getCurrentLocation();
+
+private:
+
+    void set_image(std::string fileName);
+    std::vector<cv::Point> find_color(int color);
+    void init_rooms(int numb_of_rooms);
+    void paintRoom(room* theRoom, cv::Vec3i color);
+    void paintMarble(foundMarble marble);
+
+    std::vector<room*> listOfRooms;
+    std::vector<foundMarble*> foundMarbles;
+
+    cv::Point currLocation;
+    cv::Mat ori_map;
+    bool asTree;
+    
+    int image_w; // cols
+    int image_h; // rows
+};
+
+#endif // MAP_CLASS_H
