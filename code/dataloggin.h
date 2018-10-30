d20d9ef80266690ed3683582ea14cba656efd15c
marcus-grove <mgrov16@student.sdu.dk>
2018-10-30 11:10:17 +0100

    final functionality added

--- code/dataloggin.h
+++ code/dataloggin.h
@@ -13,20 +13,39 @@
 #define FILEPATH_STATS "/test_files/loggin/statistics/"
 #define FILEPATH_MARBLES "/test_files/loggin/marbles/"
 
+#define MARBLES 'm'
+#define STATS 's'
+#define ROOMS 'r'
+
 class dataloggin
 {
 public:
+    struct room
+    {
+        int roomNumber;
+        int numbOfPixels;
+        float probabilityOfMarbles;
+    };
+
     dataloggin(std::string aBaseName,int testNumb, int runNumb, char type);
 
+    // ONLY USE WHEN type == 'm'
+    void writeMarble(map_class::foundMarble marble, int numb);
+    std::vector<map_class::foundMarble> readMarbles();
+
+    // ONLY USE WHEN type == 's'
+    void writeStats(int roomNumb ,float probability);
+    std::vector<float> readStats();
+
+    // ONLY USE WHEN type == 'r'
+    void writeRoom(int roomNumb, cv::Point massCenter, int numbOfPixels);
+    std::vector<map_class::room> readRooms();
+
+private:
     void write(std::string text);
     std::vector<std::string> read();
     void erase();
 
-    void writeMarble(map_class::foundMarble marble, int numb);
-    std::vector<map_class::foundMarble> readMarbles();
-
-
-private:
     std::string filePath;
     bool firstTimeCall = true;
 };
