8d78f663e286f5430210c1be0d6cc928600a45b0
marcus-grove <mgrov16@student.sdu.dk>
2018-11-01 15:55:48 +0100

    map_class.h

--- code/map_class.h
+++ code/map_class.h
@@ -9,41 +9,19 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include "bst_coordinates.h"
+#include "ct.h"
+//#include <opencv2/opencv.hpp>
+//#include "bst_coordinates.h"
 
 class map_class
 {
 public:
-
-    struct point
-    {
-        float x;
-        float y;
-    };
-
-    struct room
-    {
-        int roomNumber;
-        std::vector<cv::Point> coordinates;
-        BST_coordinates coordinatesTree;
-        float probabilityOfMarbles;
-        int numbOfPixels;
-        cv::Point centerOfMass;
-    };
-    
-    struct foundMarble
-    {
-        cv::Point center;
-        room *foundInRoom;
-    };
-
     map_class(std::string fileName, int numb_of_rooms, bool setAsTree);
 
-    std::vector<room*> getRooms();
+    std::vector<ct::room*> getRooms();
     void showMap(std::string title);
-    cv::Point convertToPixel(point aPoint);
-    point convertToCoordinate(cv::Point pixel);
+    cv::Point convertToPixel(ct::point aPoint);
+    ct::point convertToCoordinate(cv::Point pixel);
     void setCurrentLocation(cv::Point coordinate);
     cv::Point getCurrentLocation();
 
@@ -52,11 +30,11 @@ private:
     void set_image(std::string fileName);
     std::vector<cv::Point> find_color(int color);
     void init_rooms(int numb_of_rooms);
-    void paintRoom(room* theRoom, cv::Vec3i color);
-    void paintMarble(foundMarble marble);
+    void paintRoom(ct::room* theRoom, cv::Vec3i color);
+    void paintMarble(ct::foundMarble marble);
 
-    std::vector<room*> listOfRooms;
-    std::vector<foundMarble*> foundMarbles;
+    std::vector<ct::room*> listOfRooms;
+    std::vector<ct::foundMarble*> foundMarbles;
 
     cv::Point currLocation;
     cv::Mat ori_map;
